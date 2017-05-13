724f26d293f6b98231cec9cba4536fc30237ffb8
Brian Schwind <brianmschwind@gmail.com>
2017-05-13 17:00:41 +0900

    Add extra CLI flags, simplify delay code

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate tokio_proto;
 extern crate tokio_service;
 extern crate tokio_timer;
 extern crate byteorder;
+#[macro_use]
 extern crate clap;
 extern crate rand;
 
@@ -24,6 +25,7 @@ use byteorder::{ByteOrder, LittleEndian};
 
 use tokio_core::reactor::{Core, Handle};
 use tokio_core::net::{UdpSocket, UdpCodec};
+use tokio_core::reactor::Timeout;
 
 use tokio_timer::Timer;
 
@@ -31,15 +33,17 @@ use futures::Future;
 use futures::{Sink, Stream};
 use futures::future::ok;
 use futures::sync::oneshot;
+use futures::IntoFuture;
+use futures::future::FutureResult;
 
 const MAX_PACKET_BYTES: usize = 1220;
-const MAX_CLIENTS: usize = 128;
 const SERVER_IP: &str = "127.0.0.1";
 const SERVER_PORT: u16 = 55777;
 
 mod my_adapters {
 	use futures::{Async, Stream, Poll};
 
+	// From http://stackoverflow.com/a/42470352
 	pub struct CompletionPact<S, C>
 		where S: Stream,
 			  C: Stream, 
@@ -114,15 +118,19 @@ impl UdpCodec for ClientCodec {
 	}
 }
 
-fn run_server(buf: Vec<u8>) {
-	let mut recv_counts = vec![0 as u64; MAX_CLIENTS];
+fn delay_future(duration: Duration, handle: &Handle) -> futures::Flatten<FutureResult<tokio_core::reactor::Timeout, std::io::Error>> {
+	Timeout::new(duration, &handle).into_future().flatten()
+}
+
+fn run_server(buf: Vec<u8>, num_clients: usize) {
+	let mut recv_counts = vec![0 as u64; num_clients];
 	let addr = SocketAddr::new(IpAddr::from_str(SERVER_IP).unwrap(), SERVER_PORT);
 
 	let mut core = Core::new().unwrap();
 	let handle = core.handle();
 
 	let socket = UdpSocket::bind(&addr, &handle).unwrap();
-	println!("Listening on: {}", addr);
+	println!("Listening on: {} with max of {} clients", addr, num_clients);
 
 	let (sink, stream) = socket.framed(ServerCodec).split();
 
@@ -139,7 +147,7 @@ fn run_server(buf: Vec<u8>) {
 	println!("Result: {:?}", server);
 }
 
-fn run_client(buf: &Vec<u8>, index: u16, randomize_starts: bool, run_duration: Duration, timer: Timer, handle: Handle, stop_rx: oneshot::Receiver<()>) {
+fn run_client(buf: &Vec<u8>, index: u16, randomize_starts: bool, run_duration: Duration, tick_rate_hz: u32, timer: Timer, handle: Handle, result_tx: oneshot::Sender<()>) {
 	let addr = SocketAddr::new(IpAddr::from_str("127.0.0.1").unwrap(), 0);
 	let server_addr = SocketAddr::new(IpAddr::from_str(SERVER_IP).unwrap(), SERVER_PORT);
 
@@ -151,20 +159,21 @@ fn run_client(buf: &Vec<u8>, index: u16, randomize_starts: bool, run_duration: D
 	let ret_val = Rc::new((server_addr, client_buf));
 	let (sink, stream) = socket.framed(ClientCodec).split();
 
-	let duration = Duration::from_millis(100); // 10 Hz
-	let wakeups = timer.interval(duration);
+	let send_interval_duration = Duration::from_millis(1000 / tick_rate_hz as u64);
+	let wakeups = timer.interval(send_interval_duration);
 
-	let delay_ms = if randomize_starts {
+	let send_stream = wakeups
+		.map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+
+	let delay_ms: u64 = if randomize_starts {
 		let mut rng = thread_rng();
 		rng.gen_range(0, 1000)
 	} else {
 		0
 	};
 
-	let send_stream = wakeups
-		.map_err(|e| io::Error::new(io::ErrorKind::Other, e));
-
-	let send_counter_future = my_adapters::stream_completion_pact(send_stream, stop_rx.into_stream())
+	let stop_signal = delay_future(run_duration + Duration::from_millis(delay_ms), &handle);
+	let send_counter_future = my_adapters::stream_completion_pact(send_stream, stop_signal.into_stream())
 		.fold((0 as u64, sink), move |(send_count, mut sink), _| {
 			let _ = sink.start_send(ret_val.clone()); // TODO - use this result
 
@@ -184,14 +193,16 @@ fn run_client(buf: &Vec<u8>, index: u16, randomize_starts: bool, run_duration: D
 		})
 		.map_err(|_| ());
 
+	// println!("Client {} Running at {} Hz with delay {} ms", index, 1000 / tick_rate_hz, delay_ms);
 
-	let delayed_send_counter_future = timer.sleep(Duration::from_millis(delay_ms))
+	let delayed_send_counter_future = delay_future(Duration::from_millis(delay_ms), &handle)
 		.then(move |_| {
 			send_counter_future
 		});
 
-	let dummy_stream_2 = timer.sleep(run_duration + Duration::from_millis(500)); // Give the receive loop a bit of extra time to complete
-	let read_counter_future = my_adapters::stream_completion_pact(stream, dummy_stream_2.into_stream())
+	// Give the receive loop a bit of extra time to complete
+	let read_timeout = delay_future(run_duration + Duration::from_millis(500) + Duration::from_millis(delay_ms), &handle);
+	let read_counter_future = my_adapters::stream_completion_pact(stream, read_timeout.into_stream())
 		.fold(0 as u64, move |recv_count, _| {
 			// ok(recv_count + 1) // This doesn't work, type inference fails
 			ok::<_, io::Error>(recv_count + 1)
@@ -224,45 +235,61 @@ fn main() {
 			.short("r")
 			.long("randomize")
 			.help("If set, randomizes the start times of the clients"))
+		.arg(Arg::with_name("num-clients")
+			.short("n")
+			.long("num")
+			.takes_value(true)
+			.help("Number of clients to serve or create (default 128)"))
+		.arg(Arg::with_name("duration")
+			.short("d")
+			.long("duration")
+			.takes_value(true)
+			.help("Number of seconds to run the clients (default 5)"))
+		.arg(Arg::with_name("client_tick_rate")
+			.short("t")
+			.long("tickrate")
+			.takes_value(true)
+			.help("Frequency in Hz for clients to send packets (default 10)"))
 		.get_matches();
 
 	let buf: Vec<u8> = (0..MAX_PACKET_BYTES).map(|n| n as u8).collect();
 
 	let should_run_server = matches.is_present("as-server");
 	let randomize_starts = matches.is_present("randomize-starts");
+	let num_clients = value_t!(matches, "num-clients", usize).unwrap_or(128);
+	let duration_seconds = value_t!(matches, "duration", u64).unwrap_or(5);
+	let tick_rate_hz = value_t!(matches, "client_tick_rate", u32).unwrap_or(10);
 
 	let mut core = Core::new().unwrap();
+	let handle = core.handle();
 
 	if should_run_server {
-		run_server(buf);
+		run_server(buf, num_clients);
 	} else {
-		let timer = tokio_timer::wheel().tick_duration(Duration::from_millis(30)).build();
-		let run_duration = Duration::from_millis(3000);
+		// This timer is shared so we don't create a thread per client
+		let timer = tokio_timer::wheel().tick_duration(Duration::from_millis(10)).build();
+		let run_duration = Duration::from_secs(duration_seconds);
 
 		let mut client_chans = Vec::new();
 
-		for n in 0..MAX_CLIENTS {
+		for n in 0..num_clients {
 			let (tx, rx) = oneshot::channel::<()>(); // Channel of Unit to notify clients when the test is done
-			client_chans.push(tx);
+			client_chans.push(rx);
 
-			run_client(&buf, n as u16, randomize_starts, run_duration, timer.clone(), core.handle().clone(), rx);
+			run_client(&buf, n as u16, randomize_starts, run_duration, tick_rate_hz, timer.clone(), handle.clone(), tx);
 		}
 
-		let client_delay_timeout = timer.sleep(run_duration).and_then(|_| {
-			println!("Done!");
-			
-			for transmitter in client_chans {
-				let _ = transmitter.send(());
-			}
+		let start_delay = delay_future(run_duration, &handle);
 
-			Ok(())
+		let client_delay_timeout = start_delay.and_then(|_| {
+			// Give the clients some time to print their results
+			// TODO - make this more robust
+			delay_future(Duration::from_millis(2000), &handle).then(|_| {
+				println!("Done!");
+
+				Ok(())
+			})
 		});
-
-		core.run(client_delay_timeout).unwrap();
-
-		// Give the clients some time to print their results
-		// TODO - make this more robust
-		let client_delay_timeout = timer.sleep(Duration::from_millis(1000));
 
 		core.run(client_delay_timeout).unwrap();
 	}
